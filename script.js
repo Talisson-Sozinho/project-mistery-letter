81e9e82090286019c33d97480cff73f397b21f47
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-24 21:39:34 -0300

    feito - refatoracao no requisito 16

--- script.js
+++ script.js
@@ -2,27 +2,37 @@ const cartaGerada = document.getElementById('carta-gerada');
 const inputPhrase = document.getElementById('carta-texto');
 const createLetterButton = document.getElementById('criar-carta');
 
-const stylesForRandomizer = {
-  groupStyle: ['newspaper', 'magazine1', 'magazine2'],
-  groupHeight: ['medium', 'big', 'reallybig'],
-  groupRotate: ['rotateleft', 'rotateright'],
-  groupIncline: ['skewleft', 'skewright'],
-};
+function arrayRandomClass() {
+  const stylesForRandomizer = [
+    ['newspaper', 'magazine1', 'magazine2'],
+    ['medium', 'big', 'reallybig'],
+    ['rotateleft', 'rotateright'],
+    ['skewleft', 'skewright'],
+  ];
+  const stringClassName = [];
+  for (let index = 0; index < stylesForRandomizer.length; index += 1) {
+    const randomPosition = Math.round(Math.random() * (stylesForRandomizer[index].length - 1));
+    console.log(randomPosition);
+    stringClassName.push(stylesForRandomizer[index][randomPosition]);
+  }
+  return stringClassName.join(' ');
+}
+
+function generatePhrase(phrase) {
+  cartaGerada.innerHTML = null;
+  for (let index = 0; index < phrase.length; index += 1) {
+    const span = document.createElement('span');
+    span.innerText = phrase[index];
+    span.className = arrayRandomClass();
+    cartaGerada.appendChild(span);
+  }
+}
 
 createLetterButton.addEventListener('click', () => {
   const phrase = inputPhrase.value.split(' ');
   if (phrase.length === inputPhrase.value.length + 1) {
     cartaGerada.innerText = 'por favor, digite o conteúdo da carta.';
   } else {
-    cartaGerada.innerHTML = null;
-    for (let index = 0; index < phrase.length; index += 1) {
-      const span = document.createElement('span');
-      span.innerText = phrase[index];
-      span.classList.add(stylesForRandomizer.groupStyle[Math.round(Math.random() * (stylesForRandomizer.groupStyle.length - 1))]);
-      span.classList.add(stylesForRandomizer.groupHeight[Math.round(Math.random() * (stylesForRandomizer.groupHeight.length - 1))]);
-      span.classList.add(stylesForRandomizer.groupRotate[Math.round(Math.random() * (stylesForRandomizer.groupRotate.length - 1))]);
-      span.classList.add(stylesForRandomizer.groupIncline[Math.round(Math.random() * (stylesForRandomizer.groupIncline.length - 1))]);
-      cartaGerada.appendChild(span);
-    }
+    generatePhrase(phrase);
   }
 });
